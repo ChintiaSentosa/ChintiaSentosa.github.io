3cf40525a20077f2d9c75f72cef1d89c6c5f4103
Chintia Sentosa <chintia.sentosa.88@gmail.com>
2021-11-22 17:11:31 +0800

    update praktikum 8

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -2,26 +2,27 @@ const key = '4b9f13d42f4588f3294e792eb518c0af';
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+function getListMovie(services, sectiontitle) {
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
-    // Objek/array JavaScript dari response.json() masuk lewat data.
-    // Menyusun komponen card movie secara dinamis
+    
 
     var moviesHTML = "";
     data.results.forEach(function(movie) {
         moviesHTML =  moviesHTML + `
         <div class="col m3 s6">
-            <div class="card">
+            <div class="card moviecard">
                 <a href="./movie.html?id=${movie.id}">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
                     </div>
                 </a>
                 <div class="card-content text-center">
-                    <strong>${movie.title}</strong>
+                <strong>${movie.title}</strong><br>
+                <strong>Release Date : ${movie.release_date}</strong>
+                
                 </div>
             </div>
         </div>
@@ -29,6 +30,8 @@ function getListMovie() {
     });
 
     document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
     })
     .catch(error);
 }
